b36fe72e50df07b193c89bcfbd38c48c6c24539c
Darin <darin@DarindeMacBook-Pro.local>
2022-01-16 13:13:07 +0800

    Initial commit

---
+++ src/com/jdbc/utils/DruidUtils.java
@@ -0,0 +1,49 @@
+package com.jdbc.utils;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DruidUtils {
+    public static DruidDataSource druidDataSource;
+
+    static {
+        try {
+            InputStream is = DruidUtils.class.getResourceAsStream("druid.properties");
+            Properties properties = new Properties();
+            properties.load(is);
+
+            druidDataSource = (DruidDataSource) DruidDataSourceFactory.createDataSource(properties);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public  static DataSource dataSource(){
+        return druidDataSource;
+    }
+
+    public static Connection getConnection (){
+        Connection connection=null;
+        try {
+            connection=druidDataSource.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getConnection());
+    }
+
+}
